26558415f39e62a4f1a1c5ea826d27c515201428
Kris Norful <krisnorful@gmail.com>
2024-06-13 16:40:23 -0500

    complete shopping app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import { AppProvider } from './context/AppContext';
-import CartValue from './components/CartValue';
-import ExpenseList from './components/ExpenseList';
-import ItemSelected from './components/ItemSelected';
-import Location from './components/Location';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AppProvider } from "./context/AppContext";
+import CartValue from "./components/CartValue";
+import ExpenseList from "./components/ExpenseList";
+import ItemSelected from "./components/ItemSelected";
+import Location from "./components/Location";
 
 function App() {
   return (
     <div className="App">
+      <AppProvider>
+        <CartValue />
+        <Location />
+        <ExpenseList />
+        <ItemSelected />
+      </AppProvider>
     </div>
   );
 }
